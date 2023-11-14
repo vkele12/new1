b2e9e647291830a88944854442fa48b7a8a5d186
vkele12 <104184698+vkele12@users.noreply.github.com>
2023-11-14 11:41:34 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-backend-bucket-name"
+    bucket         = "amdocs-1-2023"
     key            = "deployment_roles/${TF_VAR_1}/user-management-deployment-role/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
